e11e0ca4e3d96a698bbe2276bad7ec83fb832bd5
Márcio Júnior <marcio.mj70@gmail.com>
2023-06-14 23:24:48 -0300

    feat: plug hello world

--- lib/plug_api_elixir/application.ex
+++ lib/plug_api_elixir/application.ex
@@ -1,20 +1,18 @@
 defmodule PlugApiElixir.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
+  require Logger
 
   @impl true
   def start(_type, _args) do
     children = [
-      # Starts a worker by calling: PlugApiElixir.Worker.start_link(arg)
-      # {PlugApiElixir.Worker, arg}
+      {Plug.Cowboy, scheme: :http, plug: PlugApiElixir.HelloWorldPlug, options: [port: 4000]}
     ]
-
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: PlugApiElixir.Supervisor]
+
+    Logger.info("Starting application")
+
     Supervisor.start_link(children, opts)
   end
 end
