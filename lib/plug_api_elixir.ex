130649e72f98ab15ecab95a0b22a41db3bed857a
Márcio Júnior <marcio.mj70@gmail.com>
2023-06-14 23:13:30 -0300

    initial commit

---
+++ lib/plug_api_elixir.ex
@@ -0,0 +1,18 @@
+defmodule PlugApiElixir do
+  @moduledoc """
+  Documentation for `PlugApiElixir`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PlugApiElixir.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/plug_api_elixir_test.exs
@@ -0,0 +1,8 @@
+defmodule PlugApiElixirTest do
+  use ExUnit.Case
+  doctest PlugApiElixir
+
+  test "greets the world" do
+    assert PlugApiElixir.hello() == :world
+  end
+end
